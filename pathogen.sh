4f99c70dad19f649770041fdc469b2f8a6ded78f
Markus Becker <mtb@tibyte.net>
2016-10-30 22:51:03 +0100

    fugitive

---
+++ pathogen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p ~/.vim/bundle || :
+cd ~/.vim/bundle
+
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/shawncplus/phpcomplete.vim
+git clone https://github.com/xuyuanp/nerdtree-git-plugin
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/artur-shaik/vim-javacomplete2
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/valloric/youcompleteme
+git clone https://github.com/vim-airline/vim-airline
+git clone git://github.com/tpope/vim-fugitive.git
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
