49ce4b43b04ad962e9078067b260e0f1478077b4
Markus Becker <mtb@tibyte.net>
2016-11-17 21:20:00 +0100

    nvim links

--- linkfiles.sh
+++ linkfiles.sh
@@ -12,3 +12,5 @@ l i3/i3 $HOME/.config/i3/config
 l i3/i3status $HOME/.config/i3status/config
 l zshrc.local $HOME/.zshrc.local
 l vifmrc $HOME/.vifm/vifmrc
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
--- software.sh
+++ software.sh
@@ -7,6 +7,8 @@ p() {
 p thunar
 p texmaker
 p texlive-core
+p python2-twisted
+p python2-argparse
 git clone https://go.googlesource.com/image
 cp image/font/gofont/ttfs/*.ttf /usr/share/fonts/TTF/
 rm -rf image
