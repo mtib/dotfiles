9e741cf05da3730e004e04cd3e214044b0063e23
mtib <mtb@tibyte.net>
2017-01-04 01:00:23 +0100

    make the world more colorful
    also add some y/n questions.

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config --global user.name "mtib"
 git config --global user.email "mtb@tibyte.net"
-git config --global core.editor "vim"
+git config --global core.editor "nvim"
 git config --global github.user "mtib"
 git config --global core.excludesfile "~/.gitignore"
 
